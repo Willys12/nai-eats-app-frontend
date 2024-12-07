a170748465500898acdf4d0cfaef82cf602e6e74
willys12 <ben.willys92@gmail.com>
2024-12-07 14:56:00 +0300

    Commiting changes to the frontend repo

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 
 type Props = {
     children: React.ReactNode;
-    showHero: boolean;
+    showHero?: boolean;
 }
 
 const Layout = ({ children, showHero = false }: Props) => {
